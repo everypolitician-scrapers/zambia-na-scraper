d5e5894fb4f43da13afc41b5c5961ba1a72c96c4
Tony Bowden <tony@tmtm.com>
2015-05-16 13:25:19 +0100

    Get all three pages
    This should really get the ‘next’ link each time, but for now just
    let’s hardcode them.

--- scraper.rb
+++ scraper.rb
@@ -22,29 +22,41 @@ def datefrom(date)
 end
 
 @BASE = 'http://www.parliament.gov.zm'
-url = @BASE + '/members-of-parliament'
-page = noko(url)
+
+# We should really extract these from the 'Next' links…
+pages = [
+  '/members-of-parliament',
+  '/members-of-parliament/page/1/0',
+  '/members-of-parliament/page/2/0',
+]
+
 
 added = 0
+pages.each do |page|
+  url = @BASE + page
+  warn "Fetching #{url}"
 
-page.css('div.view-members-of-parliament div.panel-display').each do |entry|
-  # binding.pry
-  party = entry.css('span.views-field-field-political-party .field-content').text.strip
-  (party_name, party_id) = party.match(/(.*) \((.*)\)/).captures
+  page = noko(url)
 
-  data = { 
-    id: entry.css('.views-field-view-node a/@href').text.split('/').last,
-    name: entry.css('.views-field-view-node a').text.split(/\s+/).join(" ").strip.gsub(/\s*,\s*MP\s*$/, ''),
-    photo: entry.css('div.field-content img/@src').text,
-    constituency: entry.css('span.views-field-field-constituency-name .field-content').text.strip,
-    party: party_name,
-    party_id: party_id,
-    source: url,
-    term: 2011,
-  }
-  puts data
-  # ScraperWiki.save_sqlite([:name, :term], data)
-  added += 1
+  page.css('div.view-members-of-parliament div.panel-display').each do |entry|
+    mp_url = entry.css('.views-field-view-node a/@href').text
+    party = entry.css('span.views-field-field-political-party .field-content').text.strip
+    (party_name, party_id) = party.match(/(.*) \((.*)\)/).captures
+
+    data = { 
+      id: mp_url.split('/').last,
+      name: entry.css('.views-field-view-node a').text.split(/\s+/).join(" ").strip.gsub(/\s*,\s*MP\s*$/, ''),
+      photo: entry.css('div.field-content img/@src').text,
+      constituency: entry.css('span.views-field-field-constituency-name .field-content').text.strip,
+      party: party_name,
+      party_id: party_id,
+      source: url,
+      term: 2011,
+    }
+    puts data
+    # ScraperWiki.save_sqlite([:name, :term], data)
+    added += 1
+  end
 end
 puts "  Added #{added}"
 
