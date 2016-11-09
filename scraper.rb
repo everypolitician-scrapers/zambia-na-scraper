aeb39a3dfc0b87af15024cd3114c23f5b5c7413b
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-11-09 17:19:46 +0000

    Refactor the scraper code to eliminate variables and align fields

--- scraper.rb
+++ scraper.rb
@@ -20,19 +20,18 @@ def datefrom(date)
   Date.parse(date)
 end
 
-@BASE = 'http://www.parliament.gov.zm'
+base = 'http://www.parliament.gov.zm'
 
-# We should really extract these from the 'Next' links…
+# We should really extract these from the 'Next' links...
 pages = [
   '/members-of-parliament',
   '/members-of-parliament/page/1/0',
   '/members-of-parliament/page/2/0',
 ]
 
-
 added = 0
 pages.each do |page|
-  url = @BASE + page
+  url = base + page
   warn "Fetching #{url}"
 
   page = MembersPage.new(url: url)
@@ -41,23 +40,17 @@ pages.each do |page|
     mp_url = page.mp_url(entry)
     mp     = MemberPage.new(url: mp_url)
 
-    party_name = page.party_name(entry)
-    party_id   = page.party_id(entry)
-
     data = {
       name:         page.name(entry),
       photo:        page.photo(entry),
       constituency: page.constituency(entry),
-      party:        party_name,
-      party_id:     party_id,
-      source: url,
-      term: 2011,
+      party:        page.party_name(entry),
+      party_id:     page.party_id(entry),
+      source:       url,
+      term:         2011,
     }.merge(mp.to_h)
-    # puts data
-    ScraperWiki.save_sqlite([:name, :term], data)
+    ScraperWiki.save_sqlite(%i(name term), data)
     added += 1
   end
 end
 puts "  Added #{added}"
-
-
