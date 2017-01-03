4d8e4a6f2abade8ddb558542ca80c275ab234720
Tony Bowden <tony@tmtm.com>
2017-01-03 16:01:40 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'scraperwiki'
 require 'nokogiri'
@@ -7,14 +8,14 @@ require 'date'
 require 'open-uri'
 require 'date'
 
-# require 'colorize'
-# require 'pry'
-# require 'csv'
-# require 'open-uri/cached'
-# OpenURI::Cache.cache_path = '.cache'
+#  require 'colorize'
+#  require 'pry'
+#  require 'csv'
+#  require 'open-uri/cached'
+#  OpenURI::Cache.cache_path = '.cache'
 
 def noko(url)
-  Nokogiri::HTML(open(url).read) 
+  Nokogiri::HTML(open(url).read)
 end
 
 def datefrom(date)
@@ -29,7 +30,6 @@ pages = [
   '/members-of-parliament/page/1/0',
   '/members-of-parliament/page/2/0',
 ]
-
 
 added = 0
 pages.each do |page|
@@ -46,23 +46,21 @@ pages.each do |page|
     party = entry.css('span.views-field-field-political-party .field-content').text.strip
     (party_name, party_id) = party.match(/(.*) \((.*)\)/).captures
 
-    data = { 
-      id: mp_url.split('/').last,
-      name: entry.css('.views-field-view-node a').text.split(/\s+/).join(" ").strip.gsub(/\s*,\s*MP\s*$/, ''),
-      photo: entry.css('div.field-content img/@src').text,
+    data = {
+      id:           mp_url.split('/').last,
+      name:         entry.css('.views-field-view-node a').text.split(/\s+/).join(' ').strip.gsub(/\s*,\s*MP\s*$/, ''),
+      photo:        entry.css('div.field-content img/@src').text,
       constituency: entry.css('span.views-field-field-constituency-name .field-content').text.strip,
-      party: party_name,
-      email: mp.css('.field-name-field-email .field-item a[@href*="parliament.gov.zm"]').text.strip,
-      birth_date: mp.css('.field-name-field-date .field-item .date-display-single/@content').text.split('T').first,
-      party_id: party_id,
-      source: url,
-      term: 2011,
+      party:        party_name,
+      email:        mp.css('.field-name-field-email .field-item a[@href*="parliament.gov.zm"]').text.strip,
+      birth_date:   mp.css('.field-name-field-date .field-item .date-display-single/@content').text.split('T').first,
+      party_id:     party_id,
+      source:       url,
+      term:         2011,
     }
-    # puts data
-    ScraperWiki.save_sqlite([:name, :term], data)
+    #  puts data
+    ScraperWiki.save_sqlite(%i(name term), data)
     added += 1
   end
 end
 puts "  Added #{added}"
-
-
