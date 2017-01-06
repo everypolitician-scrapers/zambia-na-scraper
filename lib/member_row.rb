713e9e15408c65523dc7ee3ab94cc75cc8764391
Oliver Denman <oliver@therewillbeanimals.com>
2017-01-06 12:28:13 +0000

    Extract MemberRow

---
+++ lib/member_row.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+require 'scraped'
+
+class MemberRow < Scraped::HTML
+  field :name do
+    noko.css('.views-field-view-node a').text.split(/\s+/).join(' ').strip.gsub(/\s*,\s*MP\s*$/, '')
+  end
+
+  field :photo do
+    noko.css('div.field-content img/@src').text
+  end
+
+  field :constituency do
+    noko.css('span.views-field-field-constituency-name .field-content').text.strip
+  end
+
+  field :id do
+    member_url.split('/').last
+  end
+
+  field :member_url do
+    URI.join(BASE, noko.css('.views-field-view-node a/@href').text).to_s
+  end
+
+  field :party_name do
+    party_name_and_id.first
+  end
+
+  field :party_id do
+    party_name_and_id.last
+  end
+
+  private
+
+  def party_name_and_id
+    noko.css('span.views-field-field-political-party .field-content').text.strip
+        .match(/(.*) \((.*)\)/).captures
+  end
+end
