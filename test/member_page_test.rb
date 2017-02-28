8a1ce043c29bfc0c1ec0dca82ab3073ac6e72a16
Oliver Denman <oliver@therewillbeanimals.com>
2017-02-28 12:09:36 +0000

    Test member with complete data

---
+++ test/member_page_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+require_relative './test_helper'
+require_relative '../lib/member_page.rb'
+
+describe MemberPage do
+  around { |test| VCR.use_cassette(File.basename(url), &test) }
+
+  let(:yaml_data) { YAML.load_file(subject) }
+  let(:url)       { yaml_data[:url] }
+  let(:response)  { MemberPage.new(response: Scraped::Request.new(url: url).response) }
+
+  describe 'Member with complete data' do
+    subject { 'test/data/CharlesRomelBanda.yml' }
+
+    it 'gets the expected data' do
+      response.to_h.must_equal yaml_data[:to_h]
+    end
+  end
+end
