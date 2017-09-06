2a10bfffaae8c4abbfc879aa4d26462d75eea3c6
Tony Bowden <tony@tmtm.com>
2017-09-06 12:17:21 +0100

    add a `next_page` method
    Rather than having a hard-coded list of pages to scraper, follow the
    'next' link on each page.

--- lib/members_page.rb
+++ lib/members_page.rb
@@ -10,4 +10,8 @@ class MembersPage < Scraped::HTML
       fragment row => MemberRow
     end
   end
+
+  field :next_page do
+    noko.css('.pager-next a/@href').map(&:text).first
+  end
 end
--- scraper.rb
+++ scraper.rb
@@ -2,36 +2,31 @@
 # encoding: utf-8
 # frozen_string_literal: true
 
+require 'pry'
 require 'scraperwiki'
+
 require 'require_all'
 require_rel 'lib'
 
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
 
-BASE = 'http://www.parliament.gov.zm'
-
-def scrape(h)
+def scraper(h)
   url, klass = h.to_a.first
   klass.new(response: Scraped::Request.new(url: url).response)
 end
 
-ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
-
-# We should really extract these from the 'Next' links
-pages = [
-  '/members-of-parliament',
-  '/members-of-parliament/page/1/0',
-  '/members-of-parliament/page/2/0',
-  '/members-of-parliament/page/3/0',
-]
-
-data = pages.flat_map do |page|
-  url = URI.join(BASE, page).to_s
-  scrape(url => MembersPage).member_rows.map do |row|
-    row.to_h.merge(scrape(row.source => MemberPage).to_h)
+def data_for(url)
+  return [] unless url
+  s = scraper(url => MembersPage)
+  data = s.member_rows.map do |row|
+    row.to_h.merge(scraper(row.source => MemberPage).to_h)
   end
+  data + data_for(s.next_page)
 end
 
+data = data_for('http://www.parliament.gov.zm/members-of-parliament')
 data.each { |mem| puts mem.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h } if ENV['MORPH_DEBUG']
+
+ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
 ScraperWiki.save_sqlite(%i[id], data)
